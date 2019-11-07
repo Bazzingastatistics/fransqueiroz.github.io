14abf8370d1962c013614c7fe3f32d5deeda34cc
fransqueiroz <fransergiosq@gmail.com>
2019-11-07 08:42:35 -0300

    criando parte de prob Uniforme

--- smartstats/js/index.js
+++ smartstats/js/index.js
@@ -32,4 +32,28 @@ $(function(){
      }
    });
    /*Demonstrar Valor Input Separatriz*/
+
+   /*Apresentação de Inputs de Valores Probabilidade*/
+   $('.selectLenght').click(function(){
+     if($('#selectLenght').val() == '<'){
+      $('.inputInsert').html("");
+      $('.selectLenght').css('flexDirection', 'row');
+      let inputInsert = /*html*/`<input type="text" name="" id="inputInsertSmaller">`
+      $('.inputInsert').append(inputInsert);
+     }else if ($('#selectLenght').val() == '>'){
+      $('.inputInsert').html("");
+      $('.selectLenght').css('flexDirection', 'row');
+      let inputInsert = /*html*/`<input type="text" name="" id="inputInsertBigger">`
+      $('.inputInsert').append(inputInsert);
+     }else{
+      $('.inputInsert').html("");
+      $('.selectLenght').css('flexDirection', 'column');
+      let inputInsert = /*html*/`<span>De</span><input type="text" name="" class="inputInsertMiddle" id="inputInsertMiddle">
+                                 <span>Até</span><input type="text" name="" class="inputInsertMiddle" id="inputInsertMiddle2">`
+      $('.inputInsert').css('display','block');
+      $('.inputInsert').append(inputInsert);
+     }
+   });
+   /*Apresentação de Inputs de Valores Probabilidade*/
+   
 });
