872d80ff7d06ce527d3292842bb90f49be493f04
fransqueiroz <fransergiosq@gmail.com>
2019-11-11 22:51:52 -0300

    Finalizando Interação e estilo da parte de probabilidade

--- smartstats/js/probability.js
+++ smartstats/js/probability.js
@@ -19,4 +19,39 @@ $(function(){
   console.log(interVal);
   console.log(intervalValue);
  });
+
+
+ /*Binomial*/
+ $('#btnBinomialCalc').click(function(){
+  let sampleSize = $('#sampleSize').val();
+  let success = ($('#inputSuccess').val() / 100);
+  let failure = ($('#inputFailure').val() / 100);
+  let interval = $('#selectBinomialLenght').val();
+  let event = [];
+  event.push($('#inputEvent').val());
+
+  console.log(sampleSize);
+  console.log(success);
+  console.log(failure);
+  console.log(interval);
+  console.log(event);
+ });
+
+ /*Normal*/
+
+ $('#btnNormalCalc').click(function(){
+  let mean = $('#inputMean').val();
+  let diversion = $('#inputDiversion').val();
+  let variance = $('#inputVariance').val();
+  let interval = $('#selectNormalLenght').val();
+  let value = [];
+  value.push($('#valueNormal').val());
+
+  console.log(mean);
+  console.log(diversion);
+  console.log(variance);
+  console.log(interval);
+  console.log(value);
+ });
+ 
 });
